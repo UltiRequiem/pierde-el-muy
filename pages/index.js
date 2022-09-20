864a313950f8ac491ef4b5e6e811e47ec406be19
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-09-19 19:14:04 -0500

    :bug: fix bug

--- pages/index.js
+++ pages/index.js
@@ -19,20 +19,20 @@ export default function Home({ results }) {
   console.log(input);
 
   return (
-    <>
+    <div className="">
       <h1 className="text-xl text-orange-900">trabajo en proceso</h1>
-
-      <p>Combine "muy" con un adjetivo simple y obtenga un adjetivo conciso.</p>
-
+      <p>
+        Combine &quot;muy&quot; con un adjetivo simple y obtenga un adjetivo
+        conciso.
+      </p>
       <h1 className="text-3xl font-bold underline">
         {" "}
         muy + {adjetive.standard} = {adjetive.magnified}
       </h1>
-
-      <input onChange={(e) => setInput(e.target.value)} />
-
+      ingresa tu busqueda <br />
+      <input placeholder="rapido" onChange={(e) => setInput(e.target.value)} />
+      <div></div>
       {input && <p>{findMagnified(input)}</p>}
-
       <a href="https://airtable.com/shr1Y5Mclp3WcVXoH">
         <button
           className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
@@ -41,7 +41,6 @@ export default function Home({ results }) {
           Actualiza la base de datos.
         </button>
       </a>
-
       <button
         className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         onClick={() => window.location.reload()}
@@ -49,9 +48,17 @@ export default function Home({ results }) {
       >
         Random
       </button>
+      <div></div>
+      todo
+      <ul>
+        <li>order html</li>
+        <li>fuzzy finder</li>
+        <li>input + initial load same place</li>
+        <li>pretify</li>
+      </ul>
 
-      <footer>Eliaz Bobadilla 2022</footer>
-    </>
+      <footer className="bg-blue-500 text-lg">Eliaz Bobadilla 2022</footer>
+    </div>
   );
 }
 
